c58ce244defab98b54d44acb43d2c9ff60cd6413
Droidec <derovor@gmail.com>
2016-06-25 23:30:59 +0200

    Delete recipe.cpp

--- recipe.cpp
+++
@@ -1,8 +0,0 @@
-#include "recipe.h"
-
-// Constructeur
-Recipe::Recipe(std::string new_name, std::vector<Product> new_productList)
-  {
-    this->name = new_name;
-    this->productList = new_productList;
-  }
