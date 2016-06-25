6ff7fa079eb3ab8def1bb1c4b6b3df6ff999323e
GQMP <gwenael.pallares@gmail.com>
2016-06-25 23:30:37 +0200

    Delete shoppinglist.cpp

--- shoppinglist.cpp
+++
@@ -1,9 +0,0 @@
-#include "shoppinglist.h"
-
-// Constructeur
-ShoppingList::ShoppingList(std::string new_name, std::vector<Product> new_productList, std::vector<Recipe> new_recipeList)
-  {
-    this->name = new_name;
-    this->productList = new_productList;
-    this->recipeList = new_recipeList;
-  }
